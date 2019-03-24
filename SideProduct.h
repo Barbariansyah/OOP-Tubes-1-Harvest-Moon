6e5612729f090086cea34a7378b0287ed306012d
Barbariansyah <43785141+Barbariansyah@users.noreply.github.com>
2019-03-24 16:59:13 +0700

    Add files via upload

--- SideProduct.h
+++ SideProduct.h
@@ -1,8 +1,9 @@
 #ifndef SIDE_PRODUCT_H
 #define SIDE_PRODUCT_H
-#include <string.h>
+
 
 #include "Product.h"
+#include "LinkedList.h"
 
 /**
  * Abstract base class SideProduct.
@@ -15,7 +16,7 @@ class SideProduct : public Product{
      * @param _price harga yang ditentukan dari barang yang dibuat
      * @param _name nama produk
      */
-    SideProduct(int _price, string _name);
+    SideProduct(double _price, string _name);
     /**
      * Implementasi method Print.
      * Method ini mencetak produk sesuai format side product
