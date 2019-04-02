3e5b1e9d2a682ab6458c87a9801cfebc3f709c06
barbariansyah <bariansyahi@gmail.com>
2019-04-02 14:13:19 +0700

    Header for all product

---
+++ AllProduct.h
@@ -0,0 +1,22 @@
+#ifndef _all_product_
+#define _all_product_
+
+#include "Product.h"
+#include "FarmProduct.h"
+#include "SideProduct.h"
+#include "DuckEgg.h"
+#include "GoatMilk.h"
+#include "DuckMeat.h"
+#include "RabbitMeat.h"
+#include "CowMeat.h"
+#include "GoatMeat.h"
+#include "ChickenMeat.h"
+#include "CowMilk.h"
+#include "ChickenEgg.h"
+#include "DinoEgg.h"
+#include "BeefRolade.h"
+#include "Pancake.h"
+#include "ChickenBMilk.h"
+#include "GoatCheese.h"
+
+#endif
